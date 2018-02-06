9989d0650262f9bb56e797233d32845d11ea3e1b
GenbuHase <hasegenbu@gmail.com>
2018-02-06 17:34:53 +0900

    スペースキーでのポーズ機能を実装

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ const vehicles = [];
 const food = [];
 const poison = [];
 
+let isPaused = false;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 
@@ -36,46 +38,54 @@ function mouseDragged() {
 	vehicles.push(new Vehicle(mouseX, mouseY));
 }
 
+function keyPressed (event) {
+	if (event.keyCode == 32) { //スペースキーを押した
+		isPaused = !isPaused;
+	}
+}
+
 function draw() {
-	background(51);
-	fill(255);
+	if (!isPaused) {
+		background(51);
+		fill(255);
 
-	if (random(1) < 0.1) {
-		food.push(createVector(random(width), random(height)));
-	}
-
-	if (random(1) < 0.01) {
-		poison.push(createVector(random(width), random(height)));
-	}
-
-	for (let i = 0; i < food.length; i++) {
-		fill(0, 255, 0);
-		ellipse(food[i].x, food[i].y, 8, 8);
-	}
-
-	for (let i = 0; i < poison.length; i++) {
-		fill(255, 0, 0);
-		ellipse(poison[i].x, poison[i].y, 8, 8);
-	}
-
-	for (let i = 0; i < vehicles.length; i++) {
-		vehicles[i].boundaries();
-		vehicles[i].behaviors(food, poison);
-		
-		// Call the appropriate steering behaviors for our agents
-		// vehicle.seek(food);
-		vehicles[i].update();
-		vehicles[i].display();
-
-		let newVehicle = vehicles[i].clone();
-
-		if (newVehicle != null) {
-			vehicles.push(newVehicle);
+		if (random(1) < 0.1) {
+			food.push(createVector(random(width), random(height)));
 		}
 
-		if (vehicles[i].isDead) {
-			food.push(createVector(vehicles[i].position.x, vehicles[i].position.y));
-			vehicles.splice(i, 1);
+		if (random(1) < 0.01) {
+			poison.push(createVector(random(width), random(height)));
+		}
+
+		for (let i = 0; i < food.length; i++) {
+			fill(0, 255, 0);
+			ellipse(food[i].x, food[i].y, 8, 8);
+		}
+
+		for (let i = 0; i < poison.length; i++) {
+			fill(255, 0, 0);
+			ellipse(poison[i].x, poison[i].y, 8, 8);
+		}
+
+		for (let i = 0; i < vehicles.length; i++) {
+			vehicles[i].boundaries();
+			vehicles[i].behaviors(food, poison);
+			
+			// Call the appropriate steering behaviors for our agents
+			// vehicle.seek(food);
+			vehicles[i].update();
+			vehicles[i].display();
+
+			let newVehicle = vehicles[i].clone();
+
+			if (newVehicle != null) {
+				vehicles.push(newVehicle);
+			}
+
+			if (vehicles[i].isDead) {
+				food.push(createVector(vehicles[i].position.x, vehicles[i].position.y));
+				vehicles.splice(i, 1);
+			}
 		}
 	}
 }
